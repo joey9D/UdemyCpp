261e4162fec56bf91626da8c3f776ca74cbd9530
joey9D <crstl9D@proton.me>
2025-03-24 15:46:57 +0100

    add cout for variable a and v

--- 03_ArrayPointer/3_3/ValueCopy.cc
+++ 03_ArrayPointer/3_3/ValueCopy.cc
@@ -2,12 +2,14 @@
 
 void f(int v)
 {
+    std::cout << "v= "<< v << "\n";
     std::cout << &v << '\n';
 }
 
 int main()
 {
     int a = 2;
+    std::cout << "a= " << a << "\n";
     std::cout << &a << '\n';
 
     f(a);
