7c21d8feaa1a72e5b91bfe80dfa2717b926f89cc
joey9D <crstl9D@proton.me>
2025-03-24 15:59:19 +0100

    add cout for variable a and v

--- 03_ArrayPointer/3_3/References.cc
+++ 03_ArrayPointer/3_3/References.cc
@@ -1,21 +1,24 @@
 #include <iostream>
 
+// creates copy v of variable a
 void f1(int v)
 {
     std::cout << "f1: " << v << '\n';
-    std::cout << &v << '\n';
+    std::cout << "Copy of a: " << &v << '\n';
 }
 
+// C Style: pointer
 void f2(int *const v)
 {
     std::cout << "f2: " << *v << '\n';
-    std::cout << v << '\n';
+    std::cout << "Pointer to *v to address " << v << '\n';
 }
 
+// C++ Style: reference/address. Use when copy should be avoided.
 void f3(int &v)
 {
     std::cout << "f3: " << v << '\n';
-    std::cout << &v << '\n';
+    std::cout << "Reference/address of a with &v in local function: " << &v << '\n';
 }
 
 int main()
