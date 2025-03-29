33ffd7b025b2e9fa0852ced4eae2eef4c12c870d
joey9D <crstl9D@proton.me>
2025-03-29 15:47:14 +0100

    move comments;

--- 05_Vector/RangedFor.cc
+++ 05_Vector/RangedFor.cc
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <vector>
 
-// 1: auto val : vec => int val : vec
-// 2: auto& val : vec => int& val : vec
-// 3: const auto val : vec => const int val : vec
-// 4: const auto& val : vec => const int& val : vec
 int main()
 {
     auto vec = std::vector<int>{1, 2, 3, 4, 5};
 
+    // 1: auto val : vec => int val : vec
     for (auto val : vec)
     {
         std::cout << val << " ";
     }
     std::cout << '\n';
 
+    // 2: auto& val : vec => int& val : vec
     for (auto &val : vec)
     {
         std::cout << val << " ";
     }
     std::cout << '\n';
 
+    // 3: const auto val : vec => const int val : vec
     for (const auto val : vec)
     {
         std::cout << val << " ";
     }
     std::cout << '\n';
 
+    // 4: const auto& val : vec => const int& val : vec
     for (const auto &val : vec)
     {
         std::cout << val << " ";
