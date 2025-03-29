93020f1f8d323a09d7f83fa85d39d046ee3e7e35
joey9D <crstl9D@proton.me>
2025-03-29 12:57:54 +0100

    add comments

--- 05_Vector/VectorFirstExample.cc
+++ 05_Vector/VectorFirstExample.cc
@@ -13,15 +13,17 @@ void print_vector(const std::vector<std::int32_t> &vec)
 
 int main()
 {
+    //                        create 5 int values, with value 0
     auto my_vector = std::vector<std::int32_t>(5, 0);
     print_vector(my_vector);
 
     my_vector[0] = 1337;
     print_vector(my_vector);
 
-    my_vector.push_back(42);
+    my_vector.push_back(42); // add value at the end of the vector
     print_vector(my_vector);
 
+    // remove the last value of the vector -> last in ,first out
     my_vector.pop_back();
     my_vector.pop_back();
     my_vector.pop_back();
