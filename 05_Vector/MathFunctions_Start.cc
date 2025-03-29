e913669c3454729ee099ad2705e85a32fbc5def3
joey9D <crstl9D@proton.me>
2025-03-29 13:22:21 +0100

    solve MathFunctionsStart.cc

--- 05_Vector/MathFunctions_Start.cc
+++ 05_Vector/MathFunctions_Start.cc
@@ -5,23 +5,41 @@
 // 1.) Schreibe eine Function, die einen Vector mit aufsteigenden Werten befüllt
 void fill_vector(std::vector<double> &vec)
 {
+    for (uint32_t i = 0; i < vec.size(); ++i)
+    {
+        vec[i] = i;
+    }
 }
 
 // 2.) Schreibe eine Function, die die Werte eines Vectors ausgibt
 void print_vector(const std::vector<double> &vec)
 {
+    for (uint32_t i = 0; i < vec.size(); ++i)
+    {
+        //std::cout << i << " "; // -> Fehler: gibt den Index aus nicht den Value an dem Index.
+        std::cout << vec[i] << " ";
+    }
+    std::cout << "\n";
 }
 
 // 3.) Schreibe Funktionen, die die Exponential Werte berechnet
 // verwendet std::exp, std::log aus <cmath>
 void exp_vector(std::vector<double> &vec)
 {
+    for (uint32_t i = 0; i < vec.size(); ++i)
+    {
+        vec[i] = exp(vec[i]);
+    }
 }
 
 // 3.) Schreibe Funktionen, die die Natural Logarithm Werte berechnet
 // verwendet std::exp, std::log aus <cmath>
 void log_vector(std::vector<double> &vec)
 {
+    for (uint32_t i = 0; i < vec.size(); ++i)
+    {
+        vec[i] = log(vec[i]);
+    }
 }
 
 int main()
