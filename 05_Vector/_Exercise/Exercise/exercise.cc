68b47e2efb1373b306462200a3a0c301704b4d17
joey9D <crstl9D@proton.me>
2025-03-30 16:46:16 +0200

    Exercise 2 - successfully add function max_row_values

--- 05_Vector/_Exercise/Exercise/exercise.cc
+++ 05_Vector/_Exercise/Exercise/exercise.cc
@@ -7,6 +7,18 @@
 // Exercise 2
 VectorT max_row_values(MatrixT &matrix)
 {
+    std::vector<double> res_vector(3, 0.0);
+
+    for (size_t i = 0; i != matrix.size(); i++)
+    {
+        for (size_t j = 0; j != matrix[i].size(); j++)
+        {
+            res_vector[j] = i + j;
+        }
+    }
+
+
+    return res_vector;
 }
 
 // Exercise 3
