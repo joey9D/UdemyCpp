ea336e631caca59c66e1f769e4aac22bd2089871
joey9D <crstl9D@proton.me>
2025-03-30 19:49:16 +0200

    Exercise 2 - was wrong; add correction and comments.

--- 05_Vector/_Exercise/Exercise/exercise.cc
+++ 05_Vector/_Exercise/Exercise/exercise.cc
@@ -7,14 +7,26 @@
 // Exercise 2
 VectorT max_row_values(MatrixT &matrix)
 {
-    std::vector<double> res_vector(3, 0.0);
+    //std::vector<double> res_vector(3, 0.0);
+    // better
+    std::vector<double> res_vector(matrix.size(), 0.0);
 
     for (size_t i = 0; i != matrix.size(); i++)
     {
-        for (size_t j = 0; j != matrix[i].size(); j++)
+        double active_row_max_value = matrix[i][0];
+
+        // 1 instead of 0
+        for (size_t j = 1; j != matrix[i].size(); ++j)
         {
-            res_vector[j] = static_cast<double>(i + j);
+            // wrong
+            // res_vector[j] = static_cast<double>(i + j);
+
+            if (matrix[i][j] > active_row_max_value)
+            {
+                active_row_max_value = matrix[i][j];
+            }
         }
+        res_vector[i] = active_row_max_value;
     }
 
     return res_vector;
