c59d56e3b73bcdc51c5e0a39f673dcfe75c24652
joey9D <crstl9D@proton.me>
2025-03-31 12:26:56 +0200

    start lecture; add substring

--- 06_String/String.cc
+++ 06_String/String.cc
@@ -7,15 +7,21 @@ int main()
     std::cout << s << '\n';
 
     const auto index = s.find("Hallo");
+    const auto index2 = s.find("Hell");
 
     if (index != std::string::npos)
     {
         std::cout << "Found substring!\n";
     }
+    else if (index2 != std::string::npos)
+    {
+        std::cout << "Found second substring!\n";
+    }
     else
     {
         std::cout << "Did not find substring!\n";
     }
+
 
     std::cout << s.empty() << '\n';
     std::cout << s.length() << '\n';
