c8fd45ec72bd85a2f74c96c3c49874a2dadea34d
joey9D <crstl9D@proton.me>
2025-03-31 12:07:09 +0200

    turn normal functions into template functions

--- 05_Vector/MathFunctions2.cc
+++ 05_Vector/MathFunctions2.cc
@@ -4,16 +4,19 @@
 
 // Aufgabe: Verwende ranged-based for loops für die Funktionen!
 
-void fill_vector(std::vector<double> &vec)
+// use as/with template function, to make it work with every data type(bool, int, char, ...)
+template <typename T>
+void fill_vector(std::vector<T> &vec)
 {
     for (int i = 0; auto &value : vec)
     {
-        value = static_cast<double>(i);
+        value = static_cast<T>(i); // cast from int to T, whichever data type has in the current run
         i++;
     }
 }
 
-void print_vector(const std::vector<double> &vec)
+template <typename T>
+void print_vector(const std::vector<T> &vec)
 {
     for (int i = 0; const auto &value : vec)
     {
@@ -23,7 +26,8 @@ void print_vector(const std::vector<double> &vec)
     std::cout << '\n';
 }
 
-void exp_vector(std::vector<double> &vec)
+template <typename T>
+void exp_vector(std::vector<T> &vec)
 {
     for (auto &value : vec)
     {
@@ -31,7 +35,8 @@ void exp_vector(std::vector<double> &vec)
     }
 }
 
-void log_vector(std::vector<double> &vec)
+template <typename T>
+void log_vector(std::vector<T> &vec)
 {
     for (auto &value : vec)
     {
@@ -42,6 +47,8 @@ void log_vector(std::vector<double> &vec)
 int main()
 {
     auto my_vector = std::vector<double>(3, 0.0);
+    std::vector template_vec_1 = std::vector<int>(3, 0);
+    std::vector template_vec_2 = std::vector<char>{'h', 'e', 'l', 'l'};
 
     fill_vector(my_vector);
     print_vector(my_vector);
@@ -51,6 +58,20 @@ int main()
 
     log_vector(my_vector);
     print_vector(my_vector);
+
+
+    fill_vector(template_vec_1);
+    print_vector(template_vec_1);
+
+    exp_vector(template_vec_1);
+    print_vector(template_vec_1);
+
+    log_vector(template_vec_1);
+    print_vector(template_vec_1);
+
+
+    print_vector(template_vec_2);
+
 
     return 0;
 }
