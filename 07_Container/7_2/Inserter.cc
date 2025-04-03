6c0098df4fbc5af28ac3c1f43645c1f2cd985025
joey9D <crstl9D@proton.me>
2025-04-03 15:14:17 +0200

    inserter

--- 07_Container/7_2/Inserter.cc
+++ 07_Container/7_2/Inserter.cc
@@ -11,27 +11,21 @@ int main()
     auto my_result2 = std::list<int>{};
 
     // container must implement "insert"
-    std::copy(my_vector.begin(),
-              my_vector.end(),
-              std::inserter(my_result, my_result.end()));
+    std::copy(my_vector.begin(), my_vector.end(), std::inserter(my_result, my_result.end()));
     for (const auto val : my_result)
         std::cout << val << " ";
     std::cout << '\n';
     my_result.clear();
 
     // container must implement "push_back"
-    std::copy(my_vector.begin(),
-              my_vector.end(),
-              std::back_inserter(my_result));
+    std::copy(my_vector.begin(), my_vector.end(), std::back_inserter(my_result));
     for (const auto val : my_result)
         std::cout << val << " ";
     std::cout << '\n';
     my_result.clear();
 
     // container must implement "push_front"
-    std::copy(my_vector.begin(),
-              my_vector.end(),
-              std::front_inserter(my_result2));
+    std::copy(my_vector.begin(), my_vector.end(), std::front_inserter(my_result2));
     for (const auto val : my_result2)
         std::cout << val << " ";
     std::cout << '\n';
