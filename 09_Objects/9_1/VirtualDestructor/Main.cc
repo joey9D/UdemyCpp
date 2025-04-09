990a01f122356b575d6689d94bfa81d7f68991f6
joey9D <crstl9D@proton.me>
2025-04-09 15:54:35 +0200

    add comment

--- 09_Objects/9_1/VirtualDestructor/Main.cc
+++ 09_Objects/9_1/VirtualDestructor/Main.cc
@@ -14,9 +14,15 @@ void printAllAgents(const std::vector<Agent *> &agents)
 
 int main()
 {
+    // object on heap
     auto agent1 = new Agent("A1", 0, 100, 25);
     auto player1 = new Player("P1", 1, 250, 55);
     auto npc1 = new NPC("N1", 2, 235, 41);
+
+    // object on stack
+    // Agent agent2("A1", 0, 100, 25);
+    // Player player2("P1", 1, 250, 55);
+    // NPC npc2("N1", 2, 235, 41);
 
     const auto agents = std::vector<Agent *>{agent1, player1, npc1};
     printAllAgents(agents);
