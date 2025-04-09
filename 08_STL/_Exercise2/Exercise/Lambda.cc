f5b9a22dc7ff29deccceba4c79dbc38b84e6238c
joey9D <crstl9D@proton.me>
2025-04-09 13:12:57 +0200

    Lambda fctn Exercise 1 solved

--- 08_STL/_Exercise2/Exercise/Lambda.cc
+++ 08_STL/_Exercise2/Exercise/Lambda.cc
@@ -28,7 +28,7 @@ int main()
 
     // Exercise 1
     const auto has_no_even =
-        std::none_of(my_vector.begin(), my_vector.end(), ...);
+        std::none_of(my_vector.begin(), my_vector.end(), [](const int value) { return (value % 2) == 0; });
 
     if (has_no_even)
     {
@@ -40,7 +40,7 @@ int main()
     }
 
     // Exercise 2
-    std::transform(my_vector.begin(), my_vector.end(), my_result.begin(), ...);
+    //std::transform(my_vector.begin(), my_vector.end(), my_result.begin(), ...);
 
     for (const auto &val : my_result)
     {
