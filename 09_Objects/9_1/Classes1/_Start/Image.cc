5caba21abceab5ed78d1a61effc0cfd32648205b
joey9D <crstl9D@proton.me>
2025-04-09 14:43:37 +0200

    start oop; udate .vscode

--- 09_Objects/9_1/Classes1/_Start/Image.cc
+++ 09_Objects/9_1/Classes1/_Start/Image.cc
@@ -1,3 +1,15 @@
 #include <iostream>
 
 #include "Image.h"
+
+Image::Image(const std::uint32_t width, const std::uint32_t height)
+{
+    m_width = width;
+    m_heigth = height;
+    std::cout << "Object created.\n";
+}
+
+Image::~Image()
+{
+    std::cout << "Object destroyed!\n";
+}
--- 09_Objects/9_1/Classes1/_Start/Image.h
+++ 09_Objects/9_1/Classes1/_Start/Image.h
@@ -2,4 +2,10 @@
 
 class Image
 {
+    public:
+        Image(const std::uint32_t m_width, const std::uint32_t m_heigth);
+        ~Image();
+    private:
+        std::uint32_t m_width;
+        std::uint32_t m_heigth;
 };
